40daaa46e074a6e1db7577789f69dbea852de47c
Jerry <jerry.guerrero95@gmail.com>
2022-10-14 01:24:20 -0500

    Main image in page

---
+++ components/MainImage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function MainImage() {
+    return (
+        <>
+            <Image src="https://picsum.photos/seed/picsum/600/300" width={600} height={300} alt="" />
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import MainImage from '../components/MainImage'
 
 export default function Home() {
   return (
     <div>
-
+      <MainImage />
     </div>
   )
 }
