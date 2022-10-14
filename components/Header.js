2cd64ff345687f03405ed7bc86e98958d6e9399f
Jerry <jerry.guerrero95@gmail.com>
2022-10-14 01:24:05 -0500

    Header component

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h4>Wins - 0</h4>
+            <h3>Cover Guesser</h3>
+            <button>New Game</button>
+        </header>
+    )
+}
