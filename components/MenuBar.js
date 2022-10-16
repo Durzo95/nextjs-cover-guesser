9d6a5c657de97f472d73d8f7f3c6b44025b2050d
Jerry <jerry.guerrero95@gmail.com>
2022-10-16 02:42:46 -0500

    change menu color

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -7,7 +7,7 @@ export default function MenuBar() {
             <header className='bg-dark my-5 rounded-3'>
                 <Stack direction="horizontal" gap={3}>
                     <h4 className="text-warning mx-auto py-2">Wins - 0</h4>
-                    <h3 className="text-light mx-auto py-2">Cover Guesser</h3>
+                    <h1 className="text-light mx-auto py-2">Cover Guesser</h1>
                     <h4 className="text-warning mx-auto py-2">New Game</h4>
                 </Stack>
             </header>
