47202e8bb2d28648f24e5869fc639a070acf5de3
Jerry <jerry.guerrero95@gmail.com>
2022-10-15 20:51:34 -0500

    Style the menu bar

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -4,11 +4,13 @@ import Link from 'next/link';
 export default function MenuBar() {
     return (
         <>
-            <Stack direction="horizontal" gap={3}>
-                <div className="bg-light border me-auto">First item</div>
-                <div className="bg-light border ms-auto">Second item</div>
-                <div className="bg-light border ms-auto">Third item</div>
-            </Stack>
+            <header className='bg-dark my-5 rounded-3'>
+                <Stack direction="horizontal" gap={3}>
+                    <h4 className="text-warning mx-auto py-2">Wins - 0</h4>
+                    <h3 className="text-warning mx-auto py-2">Cover Guesser</h3>
+                    <h4 className="text-warning mx-auto py-2">New Game</h4>
+                </Stack>
+            </header>
         </>
     )
 }
