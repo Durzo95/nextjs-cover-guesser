50e1eccbd04b4dfe682fde7908726bdc8da862f4
Jerry <jerry.guerrero95@gmail.com>
2022-10-15 21:01:27 -0500

    remove white border on cover container

--- components/Cover.js
+++ components/Cover.js
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container'
 export default function Cover() {
     return (
         <>
-            <Container className="text-center border rounded-3 bg-dark p-5">
+            <Container className="text-center rounded-3 bg-dark p-5">
                 <Image src="http://localhost:3000/Halo3.jpg" alt="Picture of the author" width={500} height={500} />
             </Container>
         </>
