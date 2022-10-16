ce0195e589289f3b9633288f7aac350163dbe3ea
Jerry <jerry.guerrero95@gmail.com>
2022-10-16 02:43:08 -0500

    Update cover image to use IGDB example

--- components/Cover.js
+++ components/Cover.js
@@ -7,7 +7,7 @@ export default function Cover() {
     return (
         <>
             <Container className="text-center rounded-3 bg-dark p-5">
-                <Image src="http://localhost:3000/Halo3.jpg" alt="Picture of the author" width={500} height={500} />
+                <Image src="https://images.igdb.com/igdb/image/upload/t_1080p/co2lbd.jpg" alt="Picture of the author" width={608} height={810} />
             </Container>
         </>
     )
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['picsum.photos', 'localhost'],
+    domains: ['picsum.photos', 'localhost', 'images.igdb.com'],
   },
 }
 
