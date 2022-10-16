0933959f83e448ac72b5c3d89ba1f914bd12ef19
Jerry <jerry.guerrero95@gmail.com>
2022-10-16 18:58:43 -0500

    Send data from IGDB and pass to Cover component

--- components/Cover.js
+++ components/Cover.js
@@ -1,13 +1,34 @@
-import React from 'react'
 import Image from 'next/image'
 import Container from 'react-bootstrap/Container'
+import { getGameData } from '../pages/api/GamesData'
+import { useState, useEffect } from 'react'
 
 
-export default function Cover() {
+export default function Cover({ data }) {
+    const [cover, setCover] = useState(''); // this is the state that will hold the cover image url
+    const [width, setWidth] = useState(608); // this is the state that will hold the cover image width
+    const [height, setHeight] = useState(810); // this is the state that will hold the cover image height
+    const [gameName, setGameName] = useState(''); // this is the state that will hold the game name
+    const [gameSummary, setGameSummary] = useState(''); // this is the state that will hold the game summary
+
+    console.log(data)
+    // set the state with the data from the api call once the component mounts
+    useEffect(() => {
+        // if the data is not null, set the state with the data
+        if (data) {
+            setCover(data.cover.url);
+            setWidth(data.cover.width);
+            setHeight(data.cover.height);
+            setGameName(data.name);
+            setGameSummary(data.summary);
+        }
+    }, [data])
+
     return (
         <>
             <Container className="text-center rounded-3 bg-dark p-5">
-                <Image src="https://images.igdb.com/igdb/image/upload/t_1080p/co2lbd.jpg" alt="Picture of the author" width={608} height={810} />
+                <Image src={cover} alt='Game Cover to Guess' width={width} height={height} className="border border-light" />
+                {/*width={608} height={810} */}
             </Container>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,25 @@ import MenuBar from '../components/MenuBar'
 import MainLayout from '../components/MainLayout'
 import Cover from '../components/Cover'
 import NameInput from '../components/NameInput'
+import { getGameData } from './api/GamesData'
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <>
       <MainLayout>
         <MenuBar />
-        <Cover />
+        <Cover data={data} />
         <NameInput />
       </MainLayout>
     </>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const data = await getGameData();
+
+  console.log(data);
+  return { props: { data } }
 }
 
 
