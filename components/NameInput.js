76f6ac61c65fc2f5117f216c02f270d1963641c4
Jerry <jerry.guerrero95@gmail.com>
2022-10-16 02:41:15 -0500

    basic name input

---
+++ components/NameInput.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Stack from 'react-bootstrap/Stack'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+export default function NameInput() {
+    return (
+        <Container className='bg-dark rounded-3 my-5'>
+            <Stack direction="horizontal" gap={3} className="p-3">
+                <Form.Control className="me-auto" placeholder="Guess name here..." />
+                <Button variant="secondary">Submit</Button>
+                <div className="vr" />
+                <Button variant="outline-danger">Reset</Button>
+            </Stack>
+        </Container>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import MenuBar from '../components/MenuBar'
 import MainLayout from '../components/MainLayout'
 import Cover from '../components/Cover'
+import NameInput from '../components/NameInput'
+
 export default function Home() {
   return (
     <>
       <MainLayout>
         <MenuBar />
         <Cover />
+        <NameInput />
       </MainLayout>
     </>
   )
