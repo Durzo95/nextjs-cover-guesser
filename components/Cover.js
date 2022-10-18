8888c94ef19a9dd918ad5a65c342c575b8561553
Jerry <jerry.guerrero95@gmail.com>
2022-10-18 17:42:21 -0500

    Moved hooks to parent component to share state

--- components/Cover.js
+++ components/Cover.js
@@ -1,38 +1,15 @@
 import Container from 'react-bootstrap/Container'
-import { useState, useEffect, useRef } from 'react'
 import PixelatedImage from './PixelatedImage'
-import Image from 'next/image';
 
 
-export default function Cover({ data }) {
-    const [cover, setCover] = useState(''); // this is the state that will hold the cover image url
-    const [width, setWidth] = useState(608); // this is the state that will hold the cover image width
-    const [height, setHeight] = useState(810); // this is the state that will hold the cover image height
-    const [gameName, setGameName] = useState(''); // this is the state that will hold the game name
-    const [gameSummary, setGameSummary] = useState(''); // this is the state that will hold the game summary
+export default function Cover({ coverUrl, pixelSize, pixelizeEnabled }) {
 
-    const [pixelSize, setPixelSize] = useState(10); // this is the state that will hold the pixel size
-    const [pixelizeEnabled, setPixelizeEnabled] = useState(false)
-
-    // set the state with the data from the api call once the component mounts
-    useEffect(() => {
-        // if the data is not null, set the state with the data
-        if (data) {
-            setCover(data.cover.url);
-            setWidth(data.cover.width);
-            setHeight(data.cover.height);
-            setGameName(data.name);
-            setGameSummary(data.summary);
-        }
-    }, [data])
 
     return (
         <>
             <Container className="rounded-3 bg-dark p-3" style={{ height: '50vh' }}>
-                {/* This is used to make sure the above container scales correctly with the SVG, it's bad workaround lol. Does not display on the screen*/}
-                {/* <Image src={cover} alt={gameName} width={width} height={height} className='d-none' /> */}
                 <PixelatedImage
-                    src={cover}
+                    src={coverUrl}
                     size={pixelSize}
                     enabled={pixelizeEnabled}
                 />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import MenuBar from '../components/MenuBar'
 import MainLayout from '../components/MainLayout'
 import Cover from '../components/Cover'
@@ -5,14 +7,39 @@ import NameInput from '../components/NameInput'
 import { getGameData } from './api/GamesData'
 import Stack from 'react-bootstrap/Stack'
 
+
+
 export default function Home({ data }) {
+
+  const [coverUrl, setCoverUrl] = useState(''); // this is the state that will hold the cover image url
+  const [width, setWidth] = useState(608); // this is the state that will hold the cover image width
+  const [height, setHeight] = useState(810); // this is the state that will hold the cover image height
+  const [gameName, setGameName] = useState(''); // this is the state that will hold the game name
+  const [gameSummary, setGameSummary] = useState(''); // this is the state that will hold the game summary
+
+  const [pixelSize, setPixelSize] = useState(10); // this is the state that will hold the pixel size
+  const [pixelizeEnabled, setPixelizeEnabled] = useState(true)
+
+  // set the state with the data from the api call once the component mounts
+  useEffect(() => {
+    // if the data is not null, set the state with the data
+    if (data) {
+      setCoverUrl(data.cover.url);
+      setWidth(data.cover.width);
+      setHeight(data.cover.height);
+      setGameName(data.name);
+      setGameSummary(data.summary);
+    }
+  }, [data])
+
   return (
     <>
       <MainLayout>
         <Stack direction='vertical' gap={4}>
           <MenuBar />
-          <Cover data={data} />
-          <NameInput />
+          {/* Only show the game cover once the data has been loaded */}
+          {data && <Cover coverUrl={coverUrl} pixelSize={pixelSize} pixelizeEnabled={pixelizeEnabled} />}
+          <NameInput data={{ gameName }} />
         </Stack>
       </MainLayout>
     </>
