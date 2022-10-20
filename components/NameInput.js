5c70d1f60e9954e5a49fccb4efb6cd1a490fb78c
Jerry <jerry.guerrero95@gmail.com>
2022-10-19 21:42:48 -0500

    Reformated text input to be row and columns

--- components/NameInput.js
+++ components/NameInput.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container'
 import Stack from 'react-bootstrap/Stack'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { Row, Col } from 'react-bootstrap'
 
 
 
@@ -42,10 +43,18 @@ export default function NameInput(
 
     return (
         <Container className='bg-dark rounded-3'>
-            <Stack direction="horizontal" gap={3} className="p-3">
-                <Form.Control className="me-auto" onChange={e => handleUserGuess(e)} placeholder={"Guess game name here..."} />
+            {/* <Stack direction="horizontal" gap={3} className="p-3"> */}
+            <Form.Group>
+                <Row className='p-3'>
+                    <Col xs={8} md={10}>
+                        <Form.Control type="text" placeholder="Enter Game Name" value={props.userGuess} onChange={handleUserInput} className='me-auto' />
+                    </Col>
+                    <Col xs={4} md={2}>
                 <Button variant="primary" onClick={checkGuess}>Submit</Button>
-            </Stack>
-        </Container>
+                    </Col>
+                </Row>
+            </Form.Group>
+            {/* </Stack> */}
+        </Container >
     )
 }
