b1c819c0052df2848640d8fd5501ff1409f4e4c4
Jerry <jerry.guerrero95@gmail.com>
2022-10-22 12:54:18 -0500

    remove dash

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -7,7 +7,7 @@ export default function MenuBar({ score, setRestartGame }) {
         <>
             <Container className='bg-dark rounded-3 py-2'>
                 <Stack direction="horizontal" gap={3}>
-                    <p className='text-center text-warning fs-3 fw-bold mb-0 w-25'>Wins - {score}</p>
+                    <p className='text-center text-warning fs-3 fw-bold mb-0 w-25'>Wins {score}</p>
                     <p className='text-center text-light fs-2 fw-bold mb-0 w-50'>Cover Guesser</p>
                     {/* Button to restart the game */}
                     <Button className='text-center btn btn-danger fs-5' onClick={() => setRestartGame(true)}>Restart</Button>
