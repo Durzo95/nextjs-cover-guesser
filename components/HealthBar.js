8fb3453b7a2b745aa6276a53245f504cd876cfdb
Jerry <jerry.guerrero95@gmail.com>
2022-10-22 01:06:32 -0500

    Hearts nice and centered

--- components/HealthBar.js
+++ components/HealthBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Col, Row } from 'react-bootstrap'
 import Image from 'next/image'
 // import heart.svg
 import heart from '../public/heart.svg'
@@ -8,10 +8,14 @@ export default function HealthBar({ health }) {
     return (
         <>
             <Container className='bg-dark rounded-3'>
-                {/* Display heart.svg with how much health you have */}
-                {[...Array(health)].map((h, i) =>
-                    <Image key={i} src={heart} alt='heart' />
-                )}
+                {/* Loop through and add hears centered and spaced evenly */}
+                <Row className='justify-content-center'>
+                    {Array.from(Array(health).keys()).map((i) => (
+                        <Col key={i} className='text-center'>
+                            <Image src={heart} alt='heart' />
+                        </Col>
+                    ))}
+                </Row>
             </Container>
 
         </>
