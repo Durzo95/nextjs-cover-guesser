e02516490a9bedf0c75328ca4b8470a59708330f
Jerry <jerry.guerrero95@gmail.com>
2022-10-22 23:26:28 -0500

    Using stack for hearts to keep it consistent

--- components/HealthBar.js
+++ components/HealthBar.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Container, Col, Row } from 'react-bootstrap'
+import { Container, Col, Row, Stack } from 'react-bootstrap'
 import Image from 'next/image'
-// import heart.svg
 import heart from '../public/heart.svg'
 
 export default function HealthBar({ health }) {
@@ -9,13 +8,13 @@ export default function HealthBar({ health }) {
         <>
             <Container className='bg-dark rounded-3 top-bar'>
                 {/* Loop through and add hears centered and spaced evenly */}
-                <Row className='justify-content-center'>
+                <Stack direction="horizontal">
                     {[...Array(health)].map((h, i) =>
                         <Col key={i} className='text-center'>
                             <Image src={heart} alt='heart' />
                         </Col>
                     )}
-                </Row>
+                </Stack>
             </Container>
         </>
     )
